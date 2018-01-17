10ade259c3a3206a2a0160223111457f3875a787
matthewmcneilly <matthewmcneilly182@gmail.com>
2018-01-17 15:54:58 +0000

    Add: File scripts

---
+++ Python/files.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+xmen_file = open('xmen.txt')
+
+# Prints text file line by line
+print(xmen_file.read())
+
+xmen_file.close()
---
+++ Python/files3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+xmen_file = open('xmen.txt', 'w')
+
+# Over writes files original contents
+xmen_file.write("Beast\n")
+xmen_file.write("Phoneix\n")
+
+xmen_file.close()
