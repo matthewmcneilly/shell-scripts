eb80a5af987941c6a44e102913834ded233a080d
matthewmcneilly <matthewmcneilly182@gmail.com>
2018-01-21 16:11:22 +0000

    Remove duplicates

--- Python/files2.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-
-xmen_file = open('xmen.txt')
-
-#Prints text file with newline characters
-for line in xmen_file:
-    print(line)
-
-xmen_file.close()
--- Python/files3.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-
-xmen_file = open('xmen.txt', 'w')
-
-# Over writes files original contents
-xmen_file.write("Beast\n")
-xmen_file.write("Phoneix\n")
-
-xmen_file.close()
