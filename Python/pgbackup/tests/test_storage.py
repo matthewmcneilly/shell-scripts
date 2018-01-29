4bdcc5e2ea7e857381277779b8fbb4ad2b84f2ab
matthewmcneilly <matthewmcneilly182@gmail.com>
2018-01-29 19:59:48 +0000

    Implemented S3 Storage

--- Python/pgbackup/tests/test_storage.py
+++ Python/pgbackup/tests/test_storage.py
@@ -1,17 +1,42 @@
 import tempfile
+import pytest
 
 from pgbackup import storage
 
-def test_storing_file_locally():
-    """
-    Write content from one file-like to another
-    """
+@pytest.fixture()
+def infile():
     infile = tempfile.TemporaryFile('r+')
     infile.write("Testing")
     infile.seek(0)
+    return infile
 
+
+def test_storing_file_locally(infile):
+    """
+    Write content from one file-like to another
+    """
     # Expects a file to read from and one to write to
     outfile = tempfile.NamedTemporaryFile(delete=False)
     storage.local(infile, outfile)
     with open(outfile.name) as f:
         assert f.read() == "Testing"
+
+def test_storage_file_on_s3(mocker):
+    """
+    Writes content from one file-like to s3
+    """
+    # Creates mock object client and uses it as an arg for s3
+    client = mocker.Mock()
+    mocker.patch.object(client, "upload_fileobj")
+
+    storage.s3(client,
+            infile,
+            'bucket-name',
+            'file-name')
+
+    client.upload_fileobj.assert_called_with(
+            infile,
+            "bucket-name",
+            "file-name")
+
+
